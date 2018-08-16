5cb109f7f31d017bd728c9bbbedc1501d3a995f0
Jun Ishii <jun8d.mail@gmail.com>
2018-08-17 04:39:15 +0900

    add ex06

---
+++ ch04/ex06/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func main() {
+	a := "aaaaa aaa    aa  aaaaaaa"
+	b := []byte(a)
+	c := removeDupSpace(b)
+	fmt.Println(string(c))
+}
+
+// FIXME: 「もとのスライス」
+func removeDupSpace(bs []byte) []byte {
+	result := bs[:0]
+	for i, b := range bs {
+		if unicode.IsSpace(rune(b)) {
+			if i > 0 && unicode.IsSpace(rune(bs[i-1])) {
+				continue
+			} else {
+				result = append(result, ' ')
+			}
+		} else {
+			result = append(result, b)
+		}
+	}
+	return result
+}
